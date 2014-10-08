2730e963d01dae42ac6d381edbbd8ddefaa43c12
Dmitriy Mubarakshin <dimonmub@mail.ru>
2014-10-08 21:44:03 +0400

    Initial commit

---
+++ src/Puzzle15.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Dmitriy Mubarakshin on 08.10.14.
+ */
+public class Puzzle15 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Puzzle15!");
+    }
+}
