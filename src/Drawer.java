4fb0e70796d6dce402c050a2e720ae7002a8498e
Dmitriy Mubarakshin <dimonmub@mail.ru>
2014-10-11 20:31:21 +0400

    Some game logic methods added

---
+++ src/Drawer.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Dmitriy Mubarakshin on 11.10.14.
+ */
+public class Drawer {
+
+    public void showField (GameField gf) {
+
+    }
+}
--- src/GameField.java
+++ src/GameField.java
@@ -28,4 +28,71 @@ public class GameField {
         return res;
     }
 
+    public void clickProcessing (int row, int col) {  //обработка клика
+        int dir = -1; // 0 - вверх, 1 - вниз, 2 - влево, 3 - вправо
+        if (possibleToMakeMove(row, col, dir))
+            makeMove(row, col, dir);
+        if (isGameFinished())
+            System.out.print("Поздравляем, вы победили!");
+    }
+
+    private boolean possibleToMakeMove(int row, int col, int dir) {
+        if (field[row-1][col] == 0) {
+            dir = 0;
+            return true;
+        }
+
+        if (field[row+1][col] == 0) {
+            dir = 1;
+            return true;
+        }
+
+        if (field[row][col-1] == 0) {
+            dir = 2;
+            return true;
+        }
+
+        if (field[row][col+1] == 0) {
+            dir = 3;
+            return true;
+        }
+
+        return false;
+    }
+
+    private void makeMove(int row, int col, int dir) {
+        int tmp;
+        switch (dir) {
+            case 0:
+                tmp = field[row][col];
+                field[row][col] = field[row-1][col];
+                field[row-1][col] = tmp;
+                break;
+            case 1:
+                tmp = field[row][col];
+                field[row][col] = field[row+1][col];
+                field[row+1][col] = tmp;
+                break;
+            case 2:
+                tmp = field[row][col];
+                field[row][col] = field[row][col-1];
+                field[row][col-1] = tmp;
+                break;
+            case 3:
+                tmp = field[row][col];
+                field[row][col] = field[row][col+1];
+                field[row][col+1] = tmp;
+                break;
+        }
+    }
+
+    public boolean isGameFinished() {
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++)
+                if (field[i][j] != 4*i + j + 1)
+                    return false;
+        return true;
+    }
+
+
 }
